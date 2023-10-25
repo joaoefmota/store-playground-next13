78de3e5b834b489bb5a618a9be385c185030e626
João Mota <joaoefmota@gmail.com>
2023-10-25 21:03:33 +0200

    useFetch done

--- app/api/products.ts
+++ app/api/products.ts
@@ -1,16 +1,18 @@
+import { useFetch } from "../hooks/useFetch";
 import { Product, Review } from "../types";
 
 export const getProducts = async (): Promise<Product[]> => {
-  const fetchProducts = async () => {
-    try {
-      const response = await fetch("https://fakestoreapi.com/products");
-      const data = await response.json();
-      return data;
-    } catch (error) {
-      console.log(error);
-    }
-  };
-  return await fetchProducts();
+  const { data, error } = useFetch<Product[]>(
+    "https://fakestoreapi.com/products"
+  );
+
+  if (error) {
+    throw new Error(error.message);
+  }
+  if (!data) {
+    throw new Error("No data found");
+  }
+  return data;
 };
 
 export const getProductById = async (
@@ -41,4 +43,3 @@ export const addReview = async (
   }
   return product?.rating;
 };
-
